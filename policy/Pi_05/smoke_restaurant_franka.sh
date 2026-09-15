537505fd23c1f08a6e5068dcfcf5af2b02269020
ShuoyuChen <chenshy357@mail2.sysu.edu.cn>
2026-09-15 11:16:57 +0800

    fix: share pi05 base weight cache

--- policy/Pi_05/smoke_restaurant_franka.sh
+++ policy/Pi_05/smoke_restaurant_franka.sh
@@ -9,6 +9,7 @@ LOG_ROOT="${OPENPI_LOG_ROOT:-${TRAIN_ROOT}/logs}"
 export HF_LEROBOT_HOME="${HF_LEROBOT_HOME:-${SHARED_ROOT}/datasets/lerobot}"
 export OPENPI_LEROBOT_REPO_ID="${OPENPI_LEROBOT_REPO_ID:-openskillbench/restaurant_pass_counter_franka_dense50}"
 export OPENPI_ASSETS_ROOT="${OPENPI_ASSETS_ROOT:-${TRAIN_ROOT}/assets}"
+export OPENPI_DATA_HOME="${OPENPI_DATA_HOME:-${TRAIN_ROOT}/openpi_cache}"
 export OPENPI_CHECKPOINT_ROOT="${OPENPI_SMOKE_CHECKPOINT_ROOT:-${TRAIN_ROOT}/smoke_checkpoints}"
 export OPENPI_TRAIN_CONFIG_NAME=pi05_restaurant_franka_lora_smoke
 export OPENPI_FSDP_DEVICES=1
